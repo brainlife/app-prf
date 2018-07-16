279b02050cf8eb6c93cfa88a4ffe716d8bc0ab04
Soichi Hayashi <hayashis@iu.edu>
2018-07-16 15:05:26 -0400

    recompiled with the latest version of pRF and mrTools

--- compile.sh
+++ compile.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-
 module load matlab/2017a
 
-#echo "compiling with following env"
-#env > compiled.env
+log=compiled/commit_ids.txt
+true > $log
+echo "/N/u/brlife/git/encode" >> $log
+(cd /N/u/brlife/git/encode && git log -1) >> $log
+echo "/N/u/brlife/git/vistasoft" >> $log
+(cd /N/u/brlife/git/vistasoft && git log -1) >> $log
+echo "/N/u/brlife/git/jsonlab" >> $log
+(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+echo "/N/u/kitchell/Karst/Applications/mba" >> $log
+(cd /N/u/kitchell/Karst/Applications/mba && git log -1) >> $log
+echo "/N/u/brlife/git/wma" >> $log
+(cd /N/u/brlife/git/wma && git log -1) >> $log
 
-mkdir -p compiled
+echo "compiling with following env" >> $log
+env >> $log
 
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/vistasoft'))
