0129cc38034ec3f78b4edf7e0aca22b606a05c13
Soichi Hayashi <hayashis@iu.edu>
2019-04-28 00:24:29 -0400

    padding bold path

--- create_mask.sh
+++ create_mask.sh
@@ -3,5 +3,7 @@
 set -e
 set -x
 
+echo "using $1"
+
 fslmaths aparc+aseg.nii.gz -uthr 2029 -thr 2029 aseg.visual.nii.gz 
-flirt -in aseg.visual.nii.gz -ref $bold -out mask.nii.gz
+flirt -in aseg.visual.nii.gz -ref $1 -out mask.nii.gz
